79b068eecfc75c6059eb2bb11660029921500302
Way <this.vey@gmail.com>
2014-08-09 15:59:54 +0200

    Update stackedSeriesGapFix.js

--- stackedSeriesGapFix.js
+++ stackedSeriesGapFix.js
@@ -1,3 +1,52 @@
+/**
+ * @file stackedSeriesGapFix.js
+ * @author Alexander Vey <this.vey@gmail.com>
+ * @version 1.0.0
+ * 
+ * # Summary
+ * 
+ * Fix a given list (array) of series by optimizing the rendering of gaps
+ * (null-value points) within the series in stacked area charts of [Highcharts].
+ * 
+ * ## Algorithm
+ * 
+ * - Loop over each series and collect all data gabs (points with null-values)
+ * - For each gap create two gap points that will fix the respective gap by
+ *   adding one gap point to each series right before the gap point with the timestamp
+ *   of the previous data point plus 1. On the other side add one gap
+ *   point to each series right behind the gap point with the timestamp of the next 
+ *   data point minus 1. This will expand the gap to the maximum borders of the previous
+ *   and next data points and will result in the correct visualization of that gap.
+ * 
+ * ## Example
+ * 
+ * - Imagine you have the following two data series (x = data gap - represented as null):
+ *   s1 = [1,1,1,1,1]
+ *   s2 = [2,2,x,2,2]
+ * - Without fixing these series the chart will render the both series (type area, stacked)
+ *   with the given data gaps (x) as they were 0 values. So there will be 2 triangles left
+ *   and right from the data gab due to the fact that [Highcharts] will connect the data gap
+ *   0 value with the previous and next data point.
+ * - With fixing the series the gap will be expanded by adding two more data points with 
+ *   value null to represent data gaps as well. This way [Highcharts] will connect the new
+ *   left data gap which is only 1 second behind the previous data point. The arising triangle
+ *   is invisible. The same method will hide the irritiating triangle on the right side by
+ *   adding the new data gap point just 1 second before the next data point. The gap will now be
+ *   rendered corretly between its data points.
+ *   The series s1 and s2 will be enriched:
+ *   s1 = [1,1,1,1,1,1,1]
+ *             |   |
+ *   s2 = [2,2,x,x,x,2,2]
+ *             |   |
+ *   added values (with a timestamp just 1 second before/after the next/previous data point)
+ * 
+ * ## Features:
+ * 
+ * - Designed and tested with stacked area series of [Highcharts]
+ * - AMD compatible
+ * 
+ * [Highcharts]: http://www.highcharts.com/
+ */
 (function (window, factory) {
   "use strict";
   if (typeof define === 'function' && define.amd) {
@@ -72,37 +121,11 @@
   };
   
   /**
-   * Fix the given series to correct/optimize the visualization of gaps (null-point)
-   * in Highcharts stacked area charts.
-   * Algorithm:
-   * - Loop over each series and collect all data gabs (points with null-values)
-   * - For each gap create two gap points that will fix the respective gap by
-   *   adding one gap point to each series right before the gap point with the timestamp
-   *   of the previous data point plus 1. On the other side add one gap
-   *   point to each series right behind the gap point with the timestamp of the next 
-   *   data point minus 1. This will expand the gap to the maximum borders of the previous
-   *   and next data points and will result in the correct visualization of that gap.
+   * Fix the given series (array of series) by expanding gaps (null-value points) to 
+   * optimize the rendering of the gaps in stacked area charts of Highcharts.
    * 
-   * Example:
-   * - Imagine you have the following two data series (x = data gap - represented as null):
-   *   s1 = [1,1,1,1,1]
-   *   s2 = [2,2,x,2,2]
-   * - Without fixing these series the chart will render the both series (type area, stacked)
-   *   with the given data gaps (x) as they were 0 values. So there will be 2 triangles left
-   *   and right from the data gab due to the fact that Highcharts will connect the data gap
-   *   0 value with the previous and next data point.
-   * - With fixing the series the gap will be expanded by adding two more data points with 
-   *   value null to represent data gaps as well. This way Highcharts will connect the new
-   *   left data gap which is only 1 second behind the previous data point. The arising triangle
-   *   is invisible. The same method will hide the irritiating triangle on the right side by
-   *   adding the new data gap point just 1 second before the next data point. The gap will now be
-   *   rendered corretly between its data points.
-   *   The series s1 and s2 will be enriched:
-   *   s1 = [1,1,1,1,1,1,1]
-   *             |   |
-   *   s2 = [2,2,x,x,x,2,2]
-   *             |   |
-   *   added values (with a timestamp just 1 second before/after the next/previous data point)
+   * @param {Array} series The series to be fixed
+   * @return {Object} The fixed series
    */
   var fixSeries = function(series) {
     var fixPoints = [];
@@ -139,32 +162,34 @@
 
     // loop over each series
     for (var iSeries = 0, lenSeries = series.length; iSeries < lenSeries; iSeries++) {
-        var data = series[iSeries].data;
-        
-        // Insert generated fix points into data series
-        for (var iFix = 0, lenFix = fixPoints.length; iFix < lenFix; iFix++) {
-          var fix = fixPoints[iFix];
-            
-            var prevGapValue = fix.y;
-            var postGapValue = fix.y;
-            
-            // Replace the fix null value for series that have no gap at the given point
-            if (fix.id !== series[iSeries].id) {
-                var seriePrevValue = data[fix.index - 1];
-                var seriePostValue = data[fix.index + 1];               
-                prevGapValue = (seriePrevValue && seriePrevValue.y !== null && seriePrevValue.x === fix.xPrev) ? seriePrevValue.y : fix.y;
-                postGapValue = (seriePostValue && seriePostValue.y !== null && seriePostValue.x === fix.xNext) ? seriePostValue.y : fix.y;
-            }
-            
-          if (fix.xNext) {
-            data.splice(fix.index + 1, 0, createGapPoint(fix.xNext - 1, postGapValue));
+      var data = series[iSeries].data;
+      
+      // Insert generated fix points into data series
+      for (var iFix = 0, lenFix = fixPoints.length; iFix < lenFix; iFix++) {
+        var fix = fixPoints[iFix];
+          
+          var prevGapValue = fix.y;
+          var postGapValue = fix.y;
+          
+          // Replace the fix null value for series that have no gap at the given point
+          if (fix.id !== series[iSeries].id) {
+              var seriePrevValue = data[fix.index - 1];
+              var seriePostValue = data[fix.index + 1];               
+              prevGapValue = (seriePrevValue && seriePrevValue.y !== null && seriePrevValue.x === fix.xPrev) ? seriePrevValue.y : fix.y;
+              postGapValue = (seriePostValue && seriePostValue.y !== null && seriePostValue.x === fix.xNext) ? seriePostValue.y : fix.y;
           }
-            
-          if (fix.xPrev) {
-            data.splice(fix.index, 0, createGapPoint(fix.xPrev + 1, prevGapValue));
-          }
+          
+        if (fix.xNext) {
+          data.splice(fix.index + 1, 0, createGapPoint(fix.xNext - 1, postGapValue));
         }
+          
+        if (fix.xPrev) {
+          data.splice(fix.index, 0, createGapPoint(fix.xPrev + 1, prevGapValue));
+        }
+      }
     }
+    
+    return series;
   };
   
   // Expose to public
